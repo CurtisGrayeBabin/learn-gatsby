944dd7f1a03bcdc6c92b8db09bed79952658a77e
CurtisGrayeBabin <curtsocal@gmail.com>
2021-08-22 11:14:09 -0700

    Finish Tutorial Part 2

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,16 +1,13 @@
 // Step 1: Import React
 import * as React from 'react'
-import { Link } from 'gatsby'
+import Layout from '../components/layout'
 
 // Step 2: Define your component
 const AboutPage = () => {
   return (
-    <main>
-      <title>About Me</title>
-      <h1>About Me</h1>
-      <Link to='/'>Home</Link>
-      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
-    </main>
+    <Layout pageTitle='About Page'>
+      About me page content
+    </Layout>
   )
 }
 
